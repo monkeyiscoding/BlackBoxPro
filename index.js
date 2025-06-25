93f9a4adf38ae6778ad82e19a694cac85198092e
monkeyiscoding <109468448+monkeyiscoding@users.noreply.github.com>
2025-06-25 16:34:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -36,3 +36,29 @@ videoModal.addEventListener('click', (event) => {
             allBrandsModal.classList.add('hidden');
         }
     });
+
+
+document.querySelectorAll('.menu-item').forEach(item => {
+  item.addEventListener('click', () => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    mobileMenu.classList.remove('translate-y-0');
+    mobileMenu.classList.add('-translate-y-full');
+    setTimeout(() => {
+      mobileMenu.classList.add('hidden');
+      document.body.classList.remove('overflow-hidden');
+    }, 300);
+  });
+});
+
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+  mobileMenu.classList.remove('hidden', '-translate-y-full');
+  mobileMenu.classList.add('translate-y-0');
+  document.body.classList.add('overflow-hidden');
+});
+
+document.getElementById('closeAllBrandsModal')?.addEventListener('click', () => {
+  document.getElementById('allBrandsModal')?.classList.add('hidden');
+});
